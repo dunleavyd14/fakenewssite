aa915cdf8d1464daa9c836a7c39018b31d55bfee
dunleavyd14 <31703846+dunleavyd14@users.noreply.github.com>
2019-04-29 04:28:04 -0400

    Add files via upload

---
+++ fakenews.js
@@ -0,0 +1,92 @@
+
+
+console.log(new Array(1020))
+
+
+
+window.onload = function () {
+	const vocab = ['’', 'said', 'mr.', '”', 'trump', '—', 'said.', 'would', 'one', 'new', 'people', 'also', 'like', 'could', 'president', 'two', 'first', 'it’s', 'us', 'last', 'told', 'many', 'even', 'time', 'united', 'state', 'trump’s', 'according', 'get', 'police', 'said,', 'may', 'made', 'going', '’’', 'ms.', 'years', 'make', '.', 'house', 'white', 'american', 'clinton', 'back', 'national', 'think', 'much', 'former', 'states', 'since', 'don’t', 'still', 'government', 'including', 'campaign', 'three', 'called', 'take', '”i', 'say', 'republican', 'obama', 'officials', '”the', 'way', 'see', 'another', 'want', 'know', 'news', 'political', 'around', '(cnn)', 'public', '000', 'part', 'donald', 'work', 'go', 'security', '“i', 'never', 'world', 'million', 'whether', 'found', 'york', 'long', '“the', 'year', 's.', 'federal', 'day', 'took', 'north', 'might', 'next', 'good', 'u.', 'city', 'that’s', 'law', 'need', 'among', 'court', 'used', 'came', 'company', 'come', 'use', 'asked', 'says', 'family', 'several', 'every', 'presidential', 'group', 'really', 'department', 'health', 'help', 'business', 'women', 'cnn', 'right', 'country', 'recent', 'something', 'support', 'without', 'put', 'didn’t', 'military', 'it.', 'well', 'home', 'little', 'man', 'washington', 'left', 'democratic', 'lot', 'four', 'got', 'members', 'times', 'administration', 'a.', 'least', 'party', 'russian', 'show', 'south', 'top', 'team', 'black', 'look', 'big', 'early', 'days', 'saying', 'went', 'less', 'media', 'become', 'already', 'though', 'set', 'across', '”we', 'chief', 'best', 'week', 'statement', 'often', 'end', 'office', 'foreign', 'international', 'number', 'life', 'known', 'percent', 'likely', 'making', 'working', 'case', 'far', 'republicans', 'director', 'five', 'm.', 'things', 'election', 'school', 'find', 'university', 'policy', 'great', 'killed', 'i’m', 'past', 'year,', 'high', 'later', 'executive', 'trying', 'place', 'second', 'always', 'care', 'major', 'different', 'intelligence', 'mrs.', 'seen', 'official', 'he’s', 'money', 'social', 'general', 'secretary', 'change', 'bill', 'report', 'attack', 'yet', 'better', 'hillary', 'believe', 'began', 'russia', 'able', 'justice', 'keep', 'children', 'young', 'taking', 'senate', 'call', 'men', 'away', 'doesn’t', 'power', 'trump,', 'local', 'clear', 'information', 'order', '“we', 'john', 'nearly', 'war', 'taken', 'democrats', 'vote', 'given', 'decision', 'death', '10', 'give', 'months', 'kind', 'dr.', 'real', 'video', 'wrote', 'there’s', 'using', 'friday', 'behind', 'became', 'officers', 'wanted', 'close', 'feel', 'others', 'along', 'whose', 'almost', 'move', 'investigation', 'important', 'started', 'outside', 'released', 'time,', 'point', 'led', 'deal', 'ever', 'leaders', 'enough', 'voters', 'companies', 'rights', 'however,', 'years,', 'isis', 'small', 'reported', 'night', 'economic', 'person', '..', 'tax', 'open', 'can’t', 'center', 'interview', 'china', 'human', 'comes', 'earlier', 'we’re', 'held', 'run', 'thursday', 'hard', 'getting', 'you’re', 'c.', 'thought', 'must', 'six', 'monday', 'saw', 'looking', 'private', 'tuesday', 'legal', 'it,', 'they’re', 'americans', 'defense', 'within', 'senior', 'near', 'announced', 'role', 'lost', 'hours', 'committee', 'lead', 'someone', 'questions', 'system', 'meeting', 'authorities', 'that,', 'judge', 'officer', 'late', 'plan', 'wednesday', 'european', 'clinton’s', 'live', 'expected', 'face', '2016', 'press', 'thing', 'p.', 'air', 'pay', 'despite', 'them.', 'evidence', 'attorney', 'start', 'head', 'toward', 'him.', 'worked', 'coming', 'leader', 'financial', 'control', 'woman', 'shot', 'actually', '•', 'year.', 'large', 'based', 'countries', 'now,', 'community', 'years.', 'story', 'medical', 'name', 'running', 'j.', 'received', 'attacks', 'global', 'job', 'tell', 'win', 'service', 'water', 'continue', 'makes', 'issue', 'force', 'turned', 'final', 'try', 'billion', 'full', 'trade', 'groups', 'especially', 'done', 'leave', 'potential', 'forces', 'weeks', 'tried', 'series', 'described', 'wasn’t', 'question', 'gave', 'car', 'sent', 'history', 'islamic', 'recently', '”it', '“it', 'died', 'twitter', 'talk', 'minister', 'nuclear', 'congress', 'sunday', 'plans', 'let', 'stop', 'chinese', 'march', 'hit', 'met', 'west', 'month', 'felt', 'free', 'half', 'data', 'nothing', 'i.', 'special', 'building', 'spent', 'possible', 'play', 'conservative', 'showed', 'research', 'saturday', 'week,', 'personal', 'immigration', 'appeared', 'love', 'fight', 'seemed', 'current', 'speech', 'helped', 'agency', 'syrian', 'line', 'drug', 'prime', 'program', '“it’s', 'means', 'civil', 'future', 'street', 'anything', 'middle', 'market', 'old', 'rather', 'member', 'idea', '’the', 'lives', 'senator', 'students', 'thousands', 'hope', 'inside', 'n.', 'similar', 'fire', 'michael', 'america', 'side', '”this', 'sure', 'time.', 'later,', 'efforts', 'facebook', 'added', 'issues', 'bring', '20', '”’', 'wall', 'brought', 'isn’t', 'trump.', 'f.', 'fbi', 'living', 'game', 'front', 'phone', 'central', 'fact', 'seven', 'enforcement', 'involved', 'area', 'obama’s', 'reports', 'heard', 'country’s', 'process', 'candidate', 'read', 'and,', 'probably', 'sen.', 'b.', 'key', 'act', 'turn', 'although', 'friends', 'accused', 'study', 'spokesman', 'mother', 'knew', 'college', 'following', 'strong', 'debate', '5', 'vice', 'calls', 'latest', 'spoke', 'seems', 'matter', 'played', 'states.', 'states,', 'provide', 'day,', 'shows', 'calling', 'particularly', 'supreme', 'wants', 'together', 'david', 'bad', 'james', 'father', 'conference', 'barack', 'third', 'access', 'criminal', 'email', 'instead', 'effort', 'race', 'everything', 'leading', 'message', '”it’s', 'moved', 'record', 'posted', 'british', 'travel', 'comments', 'says.', 'professor', '..’', 'perhaps', 'everyone', 'supporters', 'food', 'soon', 'san', '1,', 'include', 'county', 'climate', 'response', 'sexual', 'staff', 'raised', 'majority', 'people,', 'remain', 'longer', 'focus', 'east', 'needed', 'included', 'difficult', 'eight', 'cnn.', 'korea', 'hold', 'published', 'reporters', 'interest', 'post', 'remains', 'january', 'morning', 'syria', 'cut', 'cases', 'anyone', 'sometimes', 'biggest', 'stay', 'de', 'nominee', 'cannot', '2015', 'room', 'june', 'body', 'allowed', 'decided', 'him,', 'relationship', 'talking', 'gun', 'risk', 'sense', 'george', 'gop', 'list', 'action', 'shooting', 'terrorist', 'simply', 'president,', 'certain', 'jobs', 'share', 'believed', 'quickly', 'offered', 'parents', 'needs', 'happened', 'popular', 'border', 'paul', 'ago,', 'won’t', 'statement.', 'growing', 'single', 'online', 'chairman', 'problem', 'millions', 'return', 'french', 'experts', 'families', 'release', 'named', 'declined', 'miles', 'people.', 'whole', 'experience', 'district', 'allow', 'letter', 'understand', 'country.', 'los', '30', '“this', 'paid', 'reason', 'cost', 'increase', '“a', 'largest', 'comey', 'say,', '”a', 'book', 'organization', 'residents', 'beyond', 'event', 'position', 'suggested', 'considered', 'workers', 'moment', 'california', 'hundreds', 'charges', 'july', 'town', 'i’ve', 'looked', 'violence', 'them,', 'price', 'employees', '2014', '6', 'son', '”he', 'serious', 'failed', '”if', 'speaking', 'meet', 'age', 'significant', 'takes', 'step', 'leaving', 'services', 'served', '1', 'here’s', 'board', '_____', 'attention', 'april', 'created', 'television', 'visit', 'ask', 'nations', 'sanders', 'protect', 'space', 'source', 'build', 'previous', 'issued', 'industry', 'killing', 'either', 'al', 'bank', 'pence', 'adding', '100', 'ryan', 'that.', 'short', 'seem', 'union', 'fighting', 'stand', 'create', 'concerns', 'muslim', '15', 'offer', 'energy', 'main', 'immediately', 'added.', 'fox', 'address', 'safety', 'threat', 'continued', '2', 'ban', 'rules', 'congressional', 'd.', 'chance', 'campaign,', 'minutes', 'technology', 'playing', 'throughout', 'multiple', 'gov.', 'korean', 'largely', 'child', 'week.', 'mean', 'southern', 'day.', 'entire', 'park', 'view', 'claims', 'friday,', 'ways', 'sign', 'mark', 'red', 'kept', 'couple', 'reached', 'watch', 'higher', 'result', 'charged', 'points', 'pressure', 'president’s', 'cause', 'rest', 'cnn’s', 'primary', 'parts', 'november', 'season', 'prison', 'forward', 'mike', 'changes', 'appears', 'speak', 'level', 'signed', 'month,', 'state,', 'reach', 'hospital', 'buy', 'heart', 'florida', 'friend', 'then,', 'comment', 'goes', 'economy', 'investigators', 'bush', 'ahead', 'clinton,', '“if', 'opposition', '2015,', 'w.', '“he', 'giving', 'arrested', 'built', 'moving', 'star', 'what’s', 'victory', 'per', 'today', 'planned', 'saudi', 'common', 'apple', 'world.', 'agreement', 'victims', 'expressed', '2012', 'still,', 'iraq', 'huge', 'bit', 'lower', 'pretty', 'student', 'course', 'out.', 'flight', 'adviser', 'council', 'oil', 'candidates', 'maybe', 'campaign.', 'born', 'film', 'thursday,', 'career', 'confirmed', 'events', 'ground', '3', 'players', 'rate', 'hear', 'noted', 'words', 'areas', 'london', 'total', 'ability', 'policies', 'test', 'provided', 'problems', 'wife', 'claimed', '2014,', 'december', 'me,', 'hearing', 'powerful', 'election.', 'forced', 'mayor', 'president.', 'tuesday,', 'deputy', 'september', 'situation', 'eventually', '”but', 'february', 'version', 'night,', 'works', 'standing', 'terms', 'monday,', 'music', 'me.', 'appear', 'form', 'opened', '50', 'consider', 'claim', 'due', 'women’s', 'details', 'couldn’t', 'tv', 'capital', 'opportunity', 'fellow', 'spending', 'example,', 'emails', 'ago', 'voted', '“you', 'is,', 'search', 'documents', 'country,', 'avoid', 'project', '“but', 'politics', 'army', 'there.', 'break', 'putin', '12', 'ended', 'asking', 'firm', 'nation', 'critical', 'telling', 'ran', 'weapons', 'figure', 'we’ve', 'quite', 'fired', 'followed', 'approach', 'no.', 'world’s', 'western', 'leadership', 'model', '”and', 'nation’s', 'impact', 'light', 'lack', 'eastern', 'it,”', 'actions', 'fear', 'worth', 'she’s', 'results', 'follow', 'focused'];
+	
+
+	let pass2Model = (async (feats) => {
+		model = await tf.loadLayersModel("https://raw.githubusercontent.com/dunleavyd14/fakenewsNeuralNet/master/model4.json");
+		model.summary();
+   		
+     	let pred = await model.predict(tf.tensor2d(feats, [1, 1020]), {batchSize:1}).print();
+     	//returns a prediction [prob real, prob fake]
+
+     	console.log(pred)
+	});
+
+
+
+	let btn = document.getElementById("article-text-submit");
+	let clr = document.getElementById("article-text-clear");
+	let text = document.getElementById("article-text");
+
+	btn.onclick = function () {
+		
+		let words = text.value.toLowerCase().split(" ");
+		let feats = getVocabCounts(words);
+		console.log(words)
+		pass2Model(feats);
+	}
+
+	clr.onclick = function () {
+
+		text.value = "";
+	}
+
+	function getVocabCounts (wordarr) {
+		let a = new Array(1020).fill(0);
+
+		for(let i = 0; i < wordarr.length; i++) {
+			for (let j = 0; j< vocab.length; j++) {
+				if (wordarr[i] == vocab[j]) {
+					a[j]++;
+				}
+			}
+		}
+		return a;
+	}
+
+
+
+	
+
+}
+function setup() {
+
+  var data = [105, 212, 158, 31, 98, 54];
+  var width = 200, // canvas width and height
+      height = 350,
+      margin = 20,
+      w = width - 2 * margin, // chart area width and height
+      h = height - 2 * margin;
+  
+  var barWidth =  (h / data.length) * 0.8; // width of bar
+  var barMargin = (h / data.length) * 0.2; // margin between two bars
+  
+  createCanvas(width, height);
+  
+  textSize(14);
+  
+  push();
+  translate(margin, margin); // ignore margin area
+  
+  for(var i=0; i<data.length; i++) {
+    push();
+    fill('steelblue');
+    noStroke();
+    translate(0, i* (barWidth + barMargin)); // jump to the top right corner of the bar
+    rect(0, 0, data[i], barWidth); // draw rect
+
+    fill('#FFF');
+    text(data[i], 5, barWidth/2 + 5); // write data
+
+    pop();
+  }
+  
+  pop();
+}
+
